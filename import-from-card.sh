f20702a50afa4ddeff8690ea550816c424d17338
Dan Horst <danhorst@gmail.com>
2020-08-17 21:58:48 -0400

    Additional guards to make scripts idempotent

--- import-from-card.sh
+++ import-from-card.sh
@@ -1,9 +1,20 @@
 #! /bin/bash
 # https://superuser.com/a/1209701
 
-sudo mkdir /mnt/e
-sudo mount -t drvfs E: /mnt/e
+echo "SD Card Setup"
+if [[ -d "/mnt/d/DCIM" ]]; then
+  echo "SD Card already mounted"
+else
+  sudo mkdir -p /mnt/d
+  sudo mount -t drvfs D: /mnt/d
+fi
 
-./organize-photos.py /mnt/e/DCIM/101CANON
+echo "Drive Setup"
+if [[ -d "/mnt/f/2020" ]]; then
+  echo "Drive already mounted"
+else
+  sudo mkdir -p /mnt/f
+  sudo mount -t drvfs F: /mnt/f
+fi
 
-sudo umount /mnt/e
+sudo ./organize-photos.py /mnt/d/DCIM/100CANON
