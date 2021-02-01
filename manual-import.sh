b3bbb93a75baa976ab6b453dc97c55c4e27b056e
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2021-02-01 18:56:18 -0500

    Adding script for re-processing select photos

---
+++ manual-import.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# https://superuser.com/a/1209701
+
+echo "Drive Setup"
+if [[ -d "/mnt/e/2020" ]]; then
+  echo "Drive already mounted"
+else
+  sudo mkdir -p /mnt/e
+  sudo mount -t drvfs E: /mnt/e
+fi
+
+sudo ./organize-photos.py /mnt/e/Queue
