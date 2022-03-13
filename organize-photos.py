e10dadab0f92373420584658b2bb2b6c1a8a5949
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2022-03-13 14:15:51 -0400

    Update script to run on Windows directly
    Prior to this I was running it on WSL and there were consistent problems with getting the drives mounted and unmounted properly.
    
    In the process of updating this, I added more verbose logging and support for Fuji RAW files.

--- organize-photos.py
+++ organize-photos.py
@@ -11,15 +11,17 @@ from datetime import datetime
 def photoDate(f):
   "Return the date/time on which the given photo was taken."
 
-  cDate = subprocess.check_output(['exiftool', '-args', '-CreateDate', f])
-  cDate = cDate.split('=')[1].rstrip()
+  exifOutput = subprocess.check_output(['exiftool', '-args', '-CreateDate', f])
+  exifString = exifOutput.rstrip().decode('UTF-8')
+  cDate = exifString.split('=')[1]
+  sys.stdout.write('\nDate photo was taken: %s' % cDate)
   return datetime.strptime(cDate, "%Y:%m:%d %H:%M:%S")
 
 
 ###################### Main program ########################
 
 # Where the photos are and where they're going.
-destDir = '/mnt/e'
+destDir = 'D:/'
 
 if len(sys.argv) == 2:
   sourceDir = sys.argv[1]
@@ -27,6 +29,8 @@ else:
   sourceDir = destDir + '/Queue'
 
 errorDir = destDir + '/Unsorted'
+
+sys.stdout.write('\nProcessing photos from %s' % sourceDir)
 
 # The format for the new file names.
 fmt = "%Y-%m-%d--%H-%M-%S-"
@@ -36,7 +40,8 @@ problems = []
 
 # Get all the photos in the source folder.
 photos = os.listdir(sourceDir)
-photos = [ x for x in photos if x[-4:] == '.jpg' or x[-4:] == '.JPG' or x[-4:] == '.cr2' or x[-4:] == '.CR2']
+photos = [ x for x in photos if x[-4:] == '.jpg' or x[-4:] == '.JPG' or x[-4:] == '.cr2' or x[-4:] == '.CR2' or x[-4:] == '.raf' or x[-4:] == '.RAF']
+sys.stdout.write('\nFiles found: %d' % len(photos))
 
 # Prepare to output as processing occurs
 lastMonth = 0
@@ -49,12 +54,14 @@ if not os.path.exists(errorDir):
   os.makedirs(errorDir)
 
 # Copy photos into year and month subfolders. Name the copies according to
-# their timestamps and the origional filename.
+# their timestamps and the original filename.
 for photo in photos:
-  print "Processing %s..." % photo
-  original = sourceDir + '/' + photo
+  sys.stdout.write('\nProcessing %s...' % photo)
+  original = os.path.join(sourceDir, photo)
+  sys.stdout.write('\nOriginal file: %s' % original)
   try:
     pDate = photoDate(original)
+    sys.stdout.write('\nCreated date: %s' % pDate)
     yr = pDate.year
     mo = pDate.month
 
@@ -66,23 +73,26 @@ for photo in photos:
       sys.stdout.write('.')
 
     newname = pDate.strftime(fmt) + photo
-    thisDestDir = destDir + '/%04d/%02d' % (yr, mo)
+    thisDestDir = os.path.join(destDir, '%04d' % yr, '%02d' % mo)
+    sys.stdout.write('\nDestination directory: %s' % thisDestDir)
     if not os.path.exists(thisDestDir):
       os.makedirs(thisDestDir)
 
-    duplicate = thisDestDir + '/%s' % (newname)
+    duplicate = os.path.join(thisDestDir, '%s' % (newname))
     if os.path.isfile(duplicate):
       sys.stdout.write('\nSkipping %s file already exists\n' % (duplicate))
     else:
       shutil.copy2(original, duplicate)
-  except Exception:
-    shutil.copy2(original, errorDir + '/' + photo)
+  except Exception as e:
+    sys.stdout.write('\nError processing %s\n' % original)
+    sys.stdout.write(getattr(e, 'message', repr(e)))
+    shutil.copy2(original, os.path.join(errorDir, photo))
     problems.append(photo)
   except:
     sys.exit("Execution stopped.")
 
 # Report the problem files, if any.
 if len(problems) > 0:
-  print "\nProblem files:"
-  print "\n".join(problems)
-  print "These can be found in: %s" % errorDir
+  sys.stdout.write('\nProblem files:')
+  sys.stdout.write('\n'.join(problems))
+  sys.stdout.write('These can be found in: %s' % errorDir)
